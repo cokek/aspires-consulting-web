f1c78c937ac09eb1f2f8e026329af4f54639d0ae
BluTargaryan <opeyemiyolo@gmail.com>
2026-06-10 16:19:25 +0100

    chore: comment out MARQUEE_TEXT spans in FramedImage and FramedImageClickable components to simplify layout

--- app/components/atoms/FramedImage.tsx
+++ app/components/atoms/FramedImage.tsx
@@ -9,12 +9,12 @@ const FramedImage = ({ image = DEFAULT_IMAGE, alt, className }: { image?: string
   return (
     <div className={`relative border-radius-card overflow-hidden ${className}`}>
         <Image src={image} alt={alt} width={1000} height={1000} className="w-full h-full object-cover" />
-        <span className="absolute -top-1.5 left-1/2 -translate-x-1/2 text-3xl text-background font-bold tracking-widest leading-none whitespace-nowrap overflow-hidden">
+        {/* <span className="absolute -top-1.5 left-1/2 -translate-x-1/2 text-3xl text-background font-bold tracking-widest leading-none whitespace-nowrap overflow-hidden">
           {MARQUEE_TEXT}
         </span>
         <span className="absolute -bottom-1.5 left-1/2 -translate-x-1/2 text-3xl text-background font-bold tracking-widest leading-none whitespace-nowrap overflow-hidden">
           {MARQUEE_TEXT}
-        </span>
+        </span> */}
     </div>
   )
 }
--- app/components/atoms/FramedImageClickable.tsx
+++ app/components/atoms/FramedImageClickable.tsx
@@ -9,7 +9,7 @@ const FramedImageClickable = ({ image, alt, className, imageClassName, onClick }
    <button className={`flex flex-col h-fit overflow-hidden  group border-radius-card border-0! ${className}`} onClick={onClick}>
      <div className={`relative ${imageClassName} w-full h-[320px]`}>
         <Image src={image} alt={alt} width={1000} height={1000} className={`w-full h-full object-cover ${imageClassName}`} />
-        <span className="absolute -top-1.5 left-1/2 -translate-x-1/2 text-3xl text-background font-bold tracking-widest leading-none whitespace-nowrap overflow-hidden">
+        {/* <span className="absolute -top-1.5 left-1/2 -translate-x-1/2 text-3xl text-background font-bold tracking-widest leading-none whitespace-nowrap overflow-hidden">
           {MARQUEE_TEXT}
         </span>
         <span className="absolute -bottom-1.5 left-1/2 -translate-x-1/2 text-3xl text-text font-bold tracking-widest leading-none whitespace-nowrap overflow-hidden
@@ -17,7 +17,7 @@ const FramedImageClickable = ({ image, alt, className, imageClassName, onClick }
         group-hover:text-primary
         ">
           {MARQUEE_TEXT}
-        </span>
+        </span> */}
     </div>
     <div className='flex items-center justify-center gap-3 h-12 w-full 
     bg-text text-background text-sm 
